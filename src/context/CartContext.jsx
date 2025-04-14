3ba5c3ae02f51d5e3442b4404d9e81bf9dacc73e
DeeDash <rumpelstiltskin512@gmail.com>
2025-04-14 17:23:09 +0530

    remove redundancies + install zustand

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,41 +1,47 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useCallback } from "react";
 
 export const CartContext = createContext();
 
 export default function CartProvider({ children }) {
     const [cart, setCart] = useState([]);
 
-    function addToCart(product, quantity) {
-        let newCart = [...cart];
-        const itemRef = newCart.find((item) => item.id === product.id);
+    const addToCart = useCallback(
+        (product, quantity) => {
+            let newCart = [...cart];
+            const itemRef = newCart.find((item) => item.id === product.id);
 
-        if (itemRef) {
-            itemRef.quantity += quantity;
-        } else {
-            newCart.push({
-                id: product.id,
-                title: product.title,
-                price: product.price,
-                image: product.image,
-                quantity: quantity,
-            });
-        }
-        console.log(newCart);
-        setCart(newCart);
-    }
+            if (itemRef) {
+                itemRef.quantity += quantity;
+            } else {
+                newCart.push({
+                    id: product.id,
+                    title: product.title,
+                    price: product.price,
+                    image: product.image,
+                    quantity: quantity,
+                });
+            }
+            console.log(newCart);
+            setCart(newCart);
+        },
+        [cart]
+    );
 
-    function updateCart(product) {
-        let newCart = [...cart];
-        const itemRef = newCart.find((item) => item.id === product.id);
+    const updateCart = useCallback(
+        (product) => {
+            let newCart = [...cart];
+            const itemRef = newCart.find((item) => item.id === product.id);
 
-        if (!itemRef) return;
+            if (!itemRef) return;
 
-        if (product.quantity === 0) {
-            newCart = newCart.filter((item) => item.id !== product.id);
-        } else itemRef.quantity = product.quantity;
-        console.log(newCart);
-        setCart(newCart);
-    }
+            if (product.quantity === 0) {
+                newCart = newCart.filter((item) => item.id !== product.id);
+            } else itemRef.quantity = product.quantity;
+            console.log(newCart);
+            setCart(newCart);
+        },
+        [cart]
+    );
 
     return (
         <CartContext.Provider value={{ addToCart, updateCart, cart }}>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import Header from "../sections/Header";
 import { CartContext } from "../context/CartContext";
 
